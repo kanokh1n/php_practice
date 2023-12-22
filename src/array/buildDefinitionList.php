f50afa7be545681a9d34b805cc948db410d73c56
jcat <jcat@FOTOBANK.local>
2023-12-22 14:42:31 +0300

    4 array exercise

--- src/array/buildDefinitionList.php
+++ src/array/buildDefinitionList.php
@@ -2,5 +2,19 @@
 
 function buildDefinitionList($array)
 {
-    for()
+    $string = '';
+    if($array) {
+        foreach ($array as $row) {
+            $string = $string . "<dt>" . $row[0] . "</dt>" . "<dd>" . $row[1] . "</dd>";
+        }
+        return "<dl>" . $string . "</dl>";
+    }
+    else return $array;
 }
+
+$definitions = [
+    ['Блямба', 'Выпуклость, утолщения на поверхности чего-либо'],
+    ['Бобр', 'Животное из отряда грызунов'],
+];
+
+print_r(buildDefinitionList($definitions));
